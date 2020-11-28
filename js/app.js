ae956dec488311b7e4671bb416403fb7ecc2b464
Junior <duartejusanca@gmail.com>
2020-11-28 01:36:50 +0100

    Cas d'erreur géreé, si le champs est vide, afficher une erreur :ok

--- js/app.js
+++ js/app.js
@@ -1,29 +1,34 @@
 let form = document.querySelector('#form')
+let error = document.querySelector(".error")
 let search = document.querySelector('#search')
-
 const selectElement = document.querySelector('#select');
 
 selectElement.addEventListener('change', (event) => {
-    const result = document.querySelector('#list');
     //result.textContent = `t'as sélectionné: ${event.target.value}`;
     //console.log(event.target.value)
+    const result = document.querySelector('#list');
     let type = event.target.value;
     console.log(type)// affichage du type: anime, manga ou person
-
 
 form.addEventListener("submit",  (event)=> {
     // Bloque l'envoi du formulaire
     event.preventDefault();
-            axios
-                .get(
-                    `https://api.jikan.moe/v3/search/${type}?q=${search.value}`
-                ).then((response) => {
-                console.log(response.data)
-                let data = response.data.results
-                //console.log(data[0].title)
-                //console.log(data)
-                for(const manga of data) {
-                    if (type === "anime"){
+    if (search.value === ""){
+        error.innerHTML = "Le champs est vide!"
+        error.style.display = "block"
+    }else {
+        result.innerHTML= ""
+        error.style.display= "none"
+        axios
+            .get(
+                `https://api.jikan.moe/v3/search/${type}?q=${search.value}`
+            ).then((response) => {
+            console.log(response.data)
+            let data = response.data.results
+            //console.log(data[0].title)
+            //console.log(data)
+            for (const manga of data) {
+                if (type === "anime") {
                     //console.log(manga.title)
                     let div = document.createElement("div");
                     //ma condition pour afficher soit un dessin animé soît une personnage ou autre!
@@ -48,10 +53,9 @@ form.addEventListener("submit",  (event)=> {
                     list.append(div);
 
                 } else if (type === "person") {
-                        console.log('person')
-                        let div = document.createElement("div");
-                        //ma condition pour afficher soit un dessin animé soît une personnage ou autre!
-                        div.innerHTML = `<ion-item>
+                    console.log('personnes')
+                    let div = document.createElement("div");
+                    div.innerHTML = `<ion-item>
                                             <ion-avatar slot="start">
                                             <img src="${manga.image_url}"/>
                                               </ion-avatar>
@@ -60,13 +64,26 @@ form.addEventListener("submit",  (event)=> {
                                                 <ion-card-title>${manga.alternative_names}</ion-card-title>
                                               </ion-label>
                                             </ion-item>`;
-                        let list = document.querySelector("#list");
-                        list.append(div);
-                    }else if (type === "manga") {
-                        console.log('manga')
-                        let div = document.createElement("div");
-                        //ma condition pour afficher soit un dessin animé soît une personnage ou autre!
-                        div.innerHTML = `<ion-card>
+                    let list = document.querySelector("#list");
+                    list.append(div);
+                } else if (type === "character") {
+                    console.log('personnages')
+                    let div = document.createElement("div");
+                    div.innerHTML = `<ion-card>
+                                            <img src="${manga.image_url}" style="width: 100%"/>
+                                            <ion-card-header>                             
+                                                <ion-card-title>${manga.name}</ion-card-title>
+                                                <ion-card-content>
+                                                ${manga.alternative_names}
+                                                </ion-card-content>
+                                                </ion-card-header>
+                                        </ion-card>`;
+                    let list = document.querySelector("#list");
+                    list.append(div);
+                } else if (type === "manga") {
+                    console.log('manga')
+                    let div = document.createElement("div");
+                    div.innerHTML = `<ion-card>
                                             <img src="${manga.image_url}" style="width: 100%"/>
                                             <ion-card-header>
                                                 <ion-item>   
@@ -80,10 +97,12 @@ form.addEventListener("submit",  (event)=> {
                                                 ${manga.synopsis}
                                             </ion-card-content>
                                         </ion-card>`;
-                        let list = document.querySelector("#list");
-                        list.append(div);
-                    }
+                    let list = document.querySelector("#list");
+                    list.append(div);
                 }
-            })
+            }
         })
+    }
+        })
+
     })
